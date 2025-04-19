3077b6bd2e62212f0c25e7d6331c3cd501c980a0
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2025-04-19 09:39:54 +0800

    revisit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Local 2025
 > Local development 2025
+
+## Change logs
+
+### April 19, 2025 - v2.0.0
+- rewrite localhost
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-  $directory = __DIR__ . str_replace("/local2025", "", $_SERVER['REQUEST_URI']);
-  include "site/index.php"; 
+
+  $DIRECTORY = str_replace("/local2025", "", $_SERVER['REQUEST_URI']);
+  $DIRECTORY = str_replace("/","\\",$DIRECTORY);
+
+  define('DIRECTORY', __DIR__ . $DIRECTORY);
+  $__SERVER = [ 'DIRECTORY' => DIRECTORY ];
+
+  $directory = DIRECTORY;
+  include "site/index.php";
+  
+  // echo "<pre>";
+  // print_r($__SERVER);
+  // print_r($_SERVER);
+  // echo "</pre>";
