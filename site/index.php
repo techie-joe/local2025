969af3edb0fe924c21fc5e910b56a391e97db08d
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2025-10-12 07:24:12 +0800

    seperate favourites

--- site/index.php
+++ site/index.php
@@ -146,13 +146,6 @@ function renderDirectoryContent($directory) {
         left: 2px;
         border-radius: 5px;
       }
-      #_favourites ._link {
-        border-width:2px;
-      }
-      #_favourites ._link._select {
-        color: black !important;
-        background-color: #e8a049 !important;
-      }
     </style>
   </head>
   <body class="_body" id="_body">
@@ -222,36 +215,7 @@ function renderDirectoryContent($directory) {
                   </nav>
 
                 </div>
-                <?php if($_SERVER['REQUEST_URI'] === '/') { ?>
-                <nav id="_favourites" class="_f featured" style="flex-basis:30%">
-                  <h3 class="_h7 _mv_x5r">Favourite</h3>
-                  <div class="_ma_x5r _idiv">
-                    <a href='/core/site' class='_link _l _border _select'>Core</a>
-                    <a href='/nova/site' class='_link _l _border _select'>Nova</a>
-                    <a href='/neo' class='_link _l _border'>Neo</a>
-                    <a href='/segio/site' class='_link _l _border'>Segio</a>
-                    <a href='/tidloo/dev/manual' class='_link _l _border'>Tidloo</a>
-                    <a href='/test-sites' class='_link _l _border'>Test Sites</a>
-                  </div>
-                  <h3 class="_h7 _mb_x5r">Ace</h3>
-                  <div class="_ma_x5r _idiv">
-                    <a href='/ace' class='_link _l _border'>Ace</a>
-                    <a href='/ace/easymenu' class='_link _l _border'>EasyMenu&trade;</a>
-                    <a href='/ace/themejs' class='_link _l _border'>ThemeJS&trade;</a>
-                    <a href='/ace/a1' class='_link _l _border'>A1</a>
-                    <a href='/ace/b1' class='_link _l _border'>B1</a>
-                    <a href='/ace/vanamir' class='_link _l _border'>Vanamir</a>
-                  </div>
-                  <h3 class="_h7 _mb_x5r">Tools</h3>
-                  <div class="_ma_x5r _idiv">
-                    <a href='/phpMyAdmin' class='_link _l _border'>PHP MyAdmin</a>
-                  </div>
-                  <h3 class="_h7 _mb_x5r">System</h3>
-                  <div class="_ma_x5r _idiv" id="_system">
-                    <a href="/local2025/site/500.html" class="_link _l">500</a><a href="/local2025/site/403.html" class="_link _l">403</a><a href="/local2025/site/404.html" class="_link _l">404</a><a href="R404" title="Simulate Real 404" class="_link _l">R404</a>
-                  </div>
-                </nav>
-                <?php } ?>
+                <?php if($_SERVER['REQUEST_URI'] === '/') { include '_favourites.php'; } ?>
               </article>
             </div>
           </main>
