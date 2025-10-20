459a0b81f84e18574ea299a3062760ba9ec98615
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2025-10-20 09:52:48 +0800

    major improvement to main function

--- _php/main.php
+++ _php/main.php
@@ -7,87 +7,68 @@ $_DIR = $_SERVER['DOCUMENT_ROOT'] . $_URI;
 
 $_HOST = $_SERVER['HTTP_HOST'];
 
-function match_dir($directory, $file)
+function match_dir($directory, $content)
 {
+  // Directory to be included
+  // $include = preg_match( $regex , $content);
   // Directory to be excluded
-  $exclude = '/^[._]|^(node_modules)$/';
-  if ($_SERVER['REQUEST_URI'] == '/') {
-    $exclude = '/^[._]|^(node_modules)$/';
-  }
-  else if ($_SERVER['REQUEST_URI'] == '/local2025/') {
-    $exclude = '/^[._]|^(node_modules|root)$/';
-  }
-  return is_dir($directory . $file)
-    && preg_match($exclude, $file) === 0;
+  $exclude = preg_match('/^[.]|^(node_modules)$/', $content) === 0;
+  return is_dir($directory . $content) && $exclude;
 }
 
-function match_file($file)
+function match_file($directory, $content)
 {
-  // Files to be included only
-  // $exclude = '/^(404\.html?|index\.(html?|php))$/';
-  $exclude = '/^(index\.(php))$/';
-  $include = '/^(.*)\.(html?|php|s?css|js(on)?|txt|md)$/';
-  return preg_match($include, $file)
-    && preg_match($exclude, $file) === 0;
+  // Files to be included
+  $include = preg_match('/^(.*)\.[A-Za-z0-9]+$/', $content);
+  // $include = preg_match('/^(.*)\.(html?|php|s?css|js(on)?|txt|md)$/', $content);
+  // Files to be excluded
+  $exclude = preg_match( '/^(index\.(html|php))$/' , $content) === 0;
+  return !is_dir($directory . $content) && $include && $exclude;
 }
 
 function getDirectoryContent($directory) {
-  
-  $parentLink = <<<HTML
-  <a href=".." title="Parent directory" class="_link _l">&middot;&middot;</a>
-  HTML;
 
+  global $_URI;
+  
+  $parentLink = '<a href=".." title="Parent directory" class="_link _l">&middot;&middot;</a>';
   $echo = '';
 
   // Open a known directory, and proceed to read its contents
   // $echo .= $directory;
-  if (is_dir($directory)) {
-    if ($dh = opendir($directory)) {
-      $dirs = [];
-      $files = [];
-      while (($file = readdir($dh)) !== false) {
-        if (match_dir($directory, $file)) {
-          $dirs[] = $file;
-        } else if (match_file($file)) {
-          $files[] = $file;
-        }
+  if (is_dir($directory) && $dh = opendir($directory)) {
+    $dirs = [];
+    $files = [];
+    while (($content = readdir($dh)) !== false) {
+      if (match_dir($directory, $content)) {
+        $dirs[] = $content;
+      } else if (match_file($directory, $content)) {
+        $files[] = $content;
       }
-      closedir($dh);
-
-      $dc = count($dirs);
-      $fc = count($files);
-
-      $echo .= '<div class="_p" id="_directory">';
-      if($_SERVER['REQUEST_URI'] != '/') { $echo .= $parentLink; }
-      if ($dc) {
-        foreach ($dirs as $dir) {
-          if ($dir) {
-            $echo .= "<a href='$dir' class='_link _l'>$dir</a>";
-          }
-        }
-      }
-      else if ($fc + $dc == 0) $echo .= '<span class="_mh _b _mono _tc_red">Empty directory</span>';
-      $echo .= '</div>';
-      // $echo .= '<hr/>';
-      $echo .= '<div class="_p" id="_files">';
-      if ($fc) {
-        foreach ($files as $file) {
-          $echo .= "<a href='$file' class='_link _l'>$file</a>";
-        }
-      }
-      $echo .= '</div>';
-    } else {
-      $echo .= '<p class="_mh"><span class="_b _mono _tc_red">Could not open directory</span></p>';
     }
+    closedir($dh);
+    $dc = count($dirs);
+    $fc = count($files);
+
+    $echo .= '<div class="_p" id="_directory">';
+    if($_URI != '/') { $echo .= $parentLink; }
+    if ($fc + $dc == 0){ $echo .= '<span class="_mh _b _mono _tc_red">Empty directory</span>'; }
+    if ($dc) { foreach ($dirs as $dir) {
+      $echo .= "<a href='$dir' class='_link _l'>$dir</a>";
+    }}
+    $echo .= '</div>';
+    // $echo .= '<hr/>';
+    $echo .= '<div class="_p" id="_files">';
+    if ($fc) { foreach ($files as $file) {
+      $echo .= "<a href='$file' class='_link _l'>$file</a>";
+    }}
+    $echo .= '</div>';
   } else {
     $echo .= '<div class="_p" id="_directory">';
     $echo .= $parentLink;
-    $echo .= '<span class="_mh _b _mono _tc_red">Invalid directory</span>';
+    $echo .= '<span class="_mh _b _mono _tc_red">Invalid access</span>';
     $echo .= '</div>';
   }
-
   return $echo;
-
 }
 
 ?>
